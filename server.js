7e1b0cc8de5ed7cbe3d3df70eb117401ef3cf6b8
Kaloli <rvellark@gmail.com>
2019-03-03 16:36:44 -0700

    server set up

---
+++ server.js
@@ -0,0 +1,23 @@
+// Place req here:
+var express = require('express')
+var bodyParser = require('body-parser')
+var methodOverride = require('method-override')
+var exphbs  = require('express-handlebars');
+var app = express()
+ 
+// override with the X-HTTP-Method-Override header in the request
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+ extended: false
+}))
+
+app.use(methodOverride('_method'));
+app.engine('handlebars',exphbs({
+defaultLayout: 'main'
+
+}));
+
+app.set('view engine','handlebars');
+
+var port = 3000;
+app.listen(port);
