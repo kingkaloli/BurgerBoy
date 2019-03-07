de99e00c146facc83c9e0279fcf43986ec250cdb
Kaloli <rvellark@gmail.com>
2019-03-06 21:39:03 -0700

    update with connections

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,8 @@ if (process.env.JAWSDB_URL) {
     host: 'localhost',
     user: 'root',
     password: process.env.password,
-    database: `burger_db`
+    database: `burger_db`,
+    port: 3306
   });
 };
 
